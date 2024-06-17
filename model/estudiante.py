352d22e7bc1ac82e34177aef0fb59e27fef8bd20
Richard Rafael Quispe Gutierrez <Richard.quispe4@unmsm.edu.pe>
2024-06-17 18:34:42 -0500

    cambios realizados

--- model/estudiante.py
+++ model/estudiante.py
@@ -13,10 +13,4 @@ class Estudiante(db.Model):
     def __init__(self, id_estudiante, id_usuario, codigo_estudiante):
         self.id_estudiante = id_estudiante
         self.id_usuario = id_usuario
-        
         self.codigo_estudiante = codigo_estudiante
-        
-        
-
-        
-        
