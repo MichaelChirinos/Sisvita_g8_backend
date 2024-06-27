896f1385253490d0ead72774c44c83b511ac128e
MichaelChirinos <166664629+MichaelChirinos@users.noreply.github.com>
2024-06-26 23:33:33 -0500

    Update tratamiento.py

--- schemas/tratamiento.py
+++ schemas/tratamiento.py
@@ -1,19 +1,9 @@
 from utils.ma import ma
 from marshmallow import fields
 
-<<<<<<< HEAD
-=======
-
->>>>>>> baf12906ccd0196cebf65f37d6a3f5182101a291
 class TratamientoSchema(ma.Schema):
     id_tipo_test = fields.Integer()
     descripcion = fields.String()
 
-<<<<<<< HEAD
 Tratamiento_schema = TratamientoSchema()
 Tratamientos_schema = TratamientoSchema(many=True)
-=======
-
-Tratamiento_schema = TratamientoSchema()
-Tratamientos_schema = TratamientoSchema(many=True)
->>>>>>> baf12906ccd0196cebf65f37d6a3f5182101a291
