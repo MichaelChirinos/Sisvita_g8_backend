4aafc4301e5661f522598c195bf027df1355965d
MichaelChirinos <166664629+MichaelChirinos@users.noreply.github.com>
2024-06-26 23:31:20 -0500

    Update medida.py

--- model/medida.py
+++ model/medida.py
@@ -1,10 +1,6 @@
 from utils.db import db
 from dataclasses import dataclass
 
-<<<<<<< HEAD
-=======
-
->>>>>>> baf12906ccd0196cebf65f37d6a3f5182101a291
 @dataclass
 class Medida(db.Model):
     __tablename__ = 'medida'
@@ -12,20 +8,9 @@ class Medida(db.Model):
     id_especialista = db.Column(db.Integer, db.ForeignKey('especialista.id_especialista'))
     descripcion = db.Column(db.String(100))
 
-<<<<<<< HEAD
-    especialista = db.relationship('Especialista', backref='medida', lazy=True)
 
+    especialista = db.relationship('Especialista', backref='medida', lazy=True)
     def __init__(self, id_medida,id_especialista, descripcion):
         self.id_medida = id_medida
         self.id_especialista = id_especialista
         self.descripcion = descripcion
-=======
-
-    especialista = db.relationship('Especialista', backref='medida', lazy=True)
-
-
-    def __init__(self, id_medida,id_especialista, descripcion):
-        self.id_medida = id_medida
-        self.id_especialista = id_especialista
-        self.descripcion = descripcion
->>>>>>> baf12906ccd0196cebf65f37d6a3f5182101a291
